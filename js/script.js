a8af8095102debfa0e92e1e7da7e3e860844d10b
LennartEdberg <lennartedb@hotmail.se>
2015-08-31 11:30:30 +0200

    La till animation för cirklarna vid load

--- js/script.js
+++ js/script.js
@@ -11,11 +11,21 @@ $(window).load(function (){
         width: '100%'}, 1000);
     $('.arrow-left-blue').delay(350).fadeIn(1200)
 
+    //Första Cirkeln
+    $('.circle-blue').delay(350).animate({
+    height:'150px',
+    width:'150px'}, 1000);
+
     //Andra linjen
     $('.line-two').delay(750).animate({
         height: '250px',
         width: '100%'}, 1000);
     $('.arrow-right-green').delay(700).fadeIn(1200)
+
+    //Andra Cirkeln
+    $('.circle-green').delay(700).animate({
+    height:'150px',
+    width:'150px'}, 1000);
 
     //Tredje linjen
     $('.line-three').delay(1250).animate({
@@ -23,11 +33,21 @@ $(window).load(function (){
         width: '100%'}, 1000);
     $('.arrow-left-red').delay(1250).fadeIn(1200)
 
+    //Tredje Cirkeln
+    $('.circle-red').delay(1250).animate({
+    height:'150px',
+    width:'150px'}, 1000);
+
     //Fjärde linjen
     $('.line-four').delay(1750).animate({
         height: '250px',
         width: '100%'}, 1000);
     $('.arrow-right-white').delay(1750).fadeIn(1200)
+
+    //Fjärde Cirkeln
+    $('.circle-white').delay(1750).animate({
+    height:'150px',
+    width:'150px'}, 1000);
 
     //Femte linjen
     $('.line-five').delay(2250).animate({
@@ -35,36 +55,19 @@ $(window).load(function (){
         width: '100%'}, 1000);
     $('.arrow-left-orange').delay(2250).fadeIn(1200)
 
+    //Femte Cirkeln
+    $('.circle-orange').delay(2250).animate({
+    height:'150px',
+    width:'150px'}, 1000);
 
     //Sjätte linjen
     $('.line-six').delay(2750).animate({
         height: '250px',
         width: '100%'}, 1000);
     $('.arrow-right-pink').delay(2750).fadeIn(1200)
-    
-    $('.circle-blue').delay(350).fadeIn(1200)
+
+    //Sjätte Cirkeln
+    $('.circle-pink').delay(2750).animate({
+    height:'150px',
+    width:'150px'}, 1000);
 });
-
-
-
-// CIRKLAR BLIR STÖRRE VID HOVER *******************
-
-/*
-$(function() {
-    $('span').on('mouseover', function() {
-        $(this).stop().animate({
-            'width': 300,
-            'height': 300,
-            'margin-left': 150
-        }, 500);
-    });
-    
-    $('span').on('mouseout', function() {
-        $(this).stop().animate({
-            'width': 150,
-            'height': 150,
-            'margin-left': 290
-        }, 500);
-    });
-})
-*/
